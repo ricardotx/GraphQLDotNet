cb093af05965b45ab5bdbb06de96c234fd515941
Ricardo Teixeira <rteixxeira86@gmail.com>
2021-01-30 21:28:37 +0000

    Add services layer

---
+++ GraphQLDotNet.Services/Contracts/IAccountService.cs
@@ -0,0 +1,21 @@
+using GraphQLDotNet.Data.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GraphQLDotNet.Services.Contracts
+{
+	public interface IAccountService
+	{
+		Task<Account> CreateAccountAsync(Account account);
+
+		Task<string> DeleteAccountAsync(Guid accountId);
+
+		Task<Account> GetAccountAsync(Guid accountId);
+
+		Task<IEnumerable<Account>> GetAccountsAsync();
+
+		Task<Account> UpdateAccountAsync(Guid accountId, Account account);
+	}
+}
