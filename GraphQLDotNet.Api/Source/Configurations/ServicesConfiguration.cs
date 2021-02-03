53650f6b19450f2ef0969f165e36463432c58164
Ricardo Teixeira <rteixxeira86@gmail.com>
2021-02-03 21:41:14 +0000

    Repository changes

--- GraphQLDotNet.Api/Source/Configurations/ServicesConfiguration.cs
+++ GraphQLDotNet.Api/Source/Configurations/ServicesConfiguration.cs
@@ -3,10 +3,12 @@ using GraphQL.Server;
 using GraphQLDotNet.Api.Source.GraphQL;
 using GraphQLDotNet.Api.Source.Resolvers;
 using GraphQLDotNet.Core.Source;
+using GraphQLDotNet.Core.Source.Repositories;
 using GraphQLDotNet.Core.Source.Resolvers;
 using GraphQLDotNet.Core.Source.Services;
 using GraphQLDotNet.Data.Source;
 using GraphQLDotNet.Data.Source.Context;
+using GraphQLDotNet.Data.Source.Repositories;
 using GraphQLDotNet.Services.Source;
 
 using Microsoft.EntityFrameworkCore;
@@ -17,36 +19,50 @@ namespace GraphQLDotNet.Api.Source.Configurations
 {
 	public static class ServicesConfiguration
 	{
+		// Database
 		public static void ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
 		{
 			string connectionStr = configuration.GetConnectionString("mysqlConString");
-			services.AddDbContext<ApplicationContext>(opt => opt.UseMySql(connectionStr, ServerVersion.AutoDetect(connectionStr)));
+			services
+				.AddDbContext<ApplicationContext>(opt => opt.UseMySql(connectionStr, ServerVersion.AutoDetect(connectionStr)));
 		}
 
+		// Graphql
 		public static void ConfigureGraphQL(this IServiceCollection services)
 		{
-			services.AddScoped<AppSchema>();
-			services.AddGraphQL()
+			services
+				.AddScoped<AppSchema>()
+				.AddGraphQL()
 				.AddSystemTextJson()
 				.AddGraphTypes(typeof(AppSchema), ServiceLifetime.Scoped).AddDataLoader();
 		}
 
+		// Grapqhl Resolvers
 		public static void ConfigureGraphQLResolvers(this IServiceCollection services)
 		{
-			services.AddScoped<IOwnerResolver, OwnerResolver>();
-			services.AddScoped<IAccountResolver, AccountResolver>();
+			services
+				.AddScoped<IOwnerResolver, OwnerResolver>()
+				.AddScoped<IAccountResolver, AccountResolver>();
 		}
 
+		// Repository
 		public static void ConfigureRepositories(this IServiceCollection services)
 		{
-			services.AddScoped<IRepository, Repository>();
+			services
+				.AddScoped<IRepository, Repository>()
+				.AddScoped<IAccountRepository, AccountRepository>()
+				.AddScoped<IOwnerRepository, OwnerRepository>()
+				.AddScoped<IRoleRepository, RoleRepository>()
+				.AddScoped<IUserRepository, UserRepository>();
 		}
 
+		// Services
 		public static void ConfigureServices(this IServiceCollection services)
 		{
-			services.AddScoped<IOwnerService, OwnerService>();
-			services.AddScoped<IAccountService, AccountService>();
-			services.AddScoped<IDataLoaderService, DataLoaderService>();
+			services
+				.AddScoped<IOwnerService, OwnerService>()
+				.AddScoped<IAccountService, AccountService>()
+				.AddScoped<IDataLoaderService, DataLoaderService>();
 		}
 	}
 }
--- GraphQLDotNet.Data/Source/Repository.cs
+++ GraphQLDotNet.Data/Source/Repository.cs
@@ -1,7 +1,6 @@
 using GraphQLDotNet.Core.Source;
 using GraphQLDotNet.Core.Source.Repositories;
 using GraphQLDotNet.Data.Source.Context;
-using GraphQLDotNet.Data.Source.Repositories;
 
 using System.Threading.Tasks;
 
@@ -10,23 +9,30 @@ namespace GraphQLDotNet.Data.Source
 	public class Repository : IRepository
 	{
 		private readonly ApplicationContext _context;
-		private AccountRepository _accountRepository;
-		private OwnerRepository _ownerRepository;
-		private RoleRepository _roleRepository;
-		private UserRepository _userRepository;
+		private IAccountRepository _accountRepository;
+		private IOwnerRepository _ownerRepository;
+		private IRoleRepository _roleRepository;
+		private IUserRepository _userRepository;
 
-		public Repository(ApplicationContext context)
+		public Repository(
+			ApplicationContext context,
+			IAccountRepository accountRepository,
+			IOwnerRepository ownerRepository,
+			IRoleRepository roleRepository,
+			IUserRepository userRepository
+		)
 		{
 			_context = context;
+			_accountRepository = accountRepository;
+			_ownerRepository = ownerRepository;
+			_roleRepository = roleRepository;
+			_userRepository = userRepository;
 		}
 
-		public IAccountRepository Account => _accountRepository = _accountRepository ?? new AccountRepository(_context);
-
-		public IOwnerRepository Owner => _ownerRepository = _ownerRepository ?? new OwnerRepository(_context);
-
-		public IRoleRepository Role => _roleRepository = _roleRepository ?? new RoleRepository(_context);
-
-		public IUserRepository User => _userRepository = _userRepository ?? new UserRepository(_context);
+		public IAccountRepository Account => _accountRepository;
+		public IOwnerRepository Owner => _ownerRepository;
+		public IRoleRepository Role => _roleRepository;
+		public IUserRepository User => _userRepository;
 
 		public void Dispose()
 		{
