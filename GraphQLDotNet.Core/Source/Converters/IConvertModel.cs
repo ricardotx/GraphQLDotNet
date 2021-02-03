0bb319ea745c9e6a8d552a52a2268eb01cac3e83
Ricardo Teixeira <rteixxeira86@gmail.com>
2021-02-03 19:02:48 +0000

    Add comment

--- GraphQLDotNet.Core/Source/Converters/IConvertModel.cs
+++ GraphQLDotNet.Core/Source/Converters/IConvertModel.cs
@@ -2,6 +2,9 @@ namespace GraphQLDotNet.Core.Source.Converters
 {
 	public interface IConvertModel<TSource, TTarget>
 	{
+		/// <summary>
+		/// Convert a ApiModel to DataModel and backwards
+		/// </summary>
 		TTarget Convert();
 	}
 }
